b41d8b2c01f04f2f0723b830a2c5e24da53fad07
Ryan Turner <ryan.e.t@gmail.com>
2017-09-16 19:35:44 -0500

    Fixed some formatting issues that I introduced

--- allstar-asterisk-install.sh
+++ allstar-asterisk-install.sh
@@ -17,36 +17,36 @@ trap 'rm -rf "$DIR"' EXIT
 apt-get update
 
 # DL AllStar master
-wget -qO- https://github.com/AllStarLink/DIAL/archive/master.tar.gz | tar xvz -C $DIR
+wget -qO- https://github.com/AllStarLink/DIAL/archive/master.tar.gz | tar xvz -C "$DIR"
 
 # install required
-$DIR/DIAL-master/scripts/required_libs.sh
+"$DIR/DIAL-master/scripts/required_libs.sh"
 
 # install build_tools
-$DIR/DIAL-master/scripts/build_tools.sh
+"$DIR/DIAL-master/scripts/build_tools.sh"
 
 # get AllSter, DAHDI and kernel headers
-$DIR/DIAL-master/scripts/get_src.sh
+"$DIR/DIAL-master/scripts/get_src.sh"
 
 # build DAHDI
-$DIR/DIAL-master/scripts/build_dahdi.sh
+"$DIR/DIAL-master/scripts/build_dahdi.sh"
 
 # patch Asterisk
-$DIR/DIAL-master/scripts/patch_asterisk.sh
+"$DIR/DIAL-master/scripts/patch_asterisk.sh"
 
 # Build Asterisk
-$DIR/DIAL-master/scripts/build_asterisk.sh
+"$DIR/DIAL-master/scripts/build_asterisk.sh"
 
 # make /dev/dsp available
 # not needed for a hub
 # Though it will not hurt anything.
-echo snd_pcm_oss >>/etc/modules
+echo snd_pcm_oss >> /etc/modules
 
 # Add asterisk to logrotate
-$DIR/DIAL-master/scripts/mk_logrotate_asterisk.sh
+"$DIR/DIAL-master/scripts/mk_logrotate_asterisk.sh"
 
 # Put user scripts into /usr/local/sbin
-cp -rf $DIR/DIAL-master/post_install/* /usr/local/sbin
+cp -rf "$DIR/DIAL-master/post_install/*" /usr/local/sbin
 cp /usr/src/astsrc-1.4.23-pre/allstar/rc.updatenodelist /usr/local/bin/rc.updatenodelist
 
 # Check this out. I think it's done by modified asterisk make file now.
@@ -55,11 +55,11 @@ cp /usr/src/astsrc-1.4.23-pre/allstar/rc.updatenodelist /usr/local/bin/rc.update
 codename=$(lsb_release -cs)
 if [[ $codename == 'jessie' ]]; then
   # start update node list on boot via systemd
-  cp $DIR/DIAL-master/systemd/updatenodelist.service /lib/systemd/system
+  cp "$DIR/DIAL-master/systemd/updatenodelist.service" /lib/systemd/system
   systemctl enable updatenodelist.service
 elif [[ $codename == 'wheezy' ]]; then
   # start update node list on boot via init.d
-  cp $DIR/DIAL-master/scripts/updatenodelist /etc/init.d
+  cp "$DIR/DIAL-master/scripts/updatenodelist" /etc/init.d
   /usr/sbin/update-rc.d updatenodelist start 50 2 3 4 5 . stop 91 2 3 4 5
 fi
 
